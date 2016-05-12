8e7925817cd08e7bc60229c038af419d624bbde2
Guillermo Parada <geparada88@gmail.com>
2016-05-12 12:22:19 +0100

    A

---
+++ PhD/Teichmann/DEXSeq_count_merge.py
@@ -0,0 +1,124 @@
+import sys
+import csv
+from collections import defaultdict
+import numpy as np
+
+
+
+cell_exon_count = defaultdict(list)
+cell_gene_count = defaultdict(list)
+exon_number_exons_per_gene = defaultdict(set)
+
+
+if __name__ == '__main__':
+
+
+	gene_lenghts  = defaultdict(int)
+
+	for row in csv.reader(open(sys.argv[1]), delimiter = '\t'):
+
+		chrom, gff_file, feature, start, end, dot1, strand, dot2, IDs = row
+
+		if feature == "exonic_part":
+
+			start = int(start)
+			end = int(end)
+
+			lenght = end - start
+
+			gene = IDs.split(" ")[-1].strip('"')
+
+			gene_lenghts[gene] += lenght
+
+
+
+	cells = [i.split(".")[0] for i in sys.argv[2:] ] 
+
+
+	library_sizes = []
+
+	for i in range(2, len(sys.argv)):
+
+		gene_count = defaultdict(int)
+
+		library_size = 0
+
+
+		for row in csv.reader(open(sys.argv[i]), delimiter = '\t'):
+
+			exon, count = row
+
+			count = int(count)
+
+			if exon[0]!="_":
+
+				cell_exon_count[exon].append(count)
+
+				gene, exon_number = exon.split(":")
+
+				gene_count[gene]+= count
+
+				exon_number_exons_per_gene[gene].add(exon_number)
+
+				library_size += count
+
+
+		library_sizes.append(library_size)
+
+		for i in gene_count.items():
+
+			gene, count =  i
+			cell_gene_count[gene].append(count)
+
+
+	for i in cell_exon_count.items():
+
+		
+
+		exon, exon_counts = i
+
+		gene, exon_number = exon.split(":")
+
+
+		gene_counts =  cell_gene_count[gene]
+
+		ratios = []
+		phis = []
+		rpkms = []
+		
+
+		for e, g, lib_size, cell in zip(exon_counts, gene_counts, library_sizes, cells):
+
+			if e!=0:
+
+
+				
+				ratio = str(e) + "/" + str(g)
+				phi= float(e)/float(g)
+				gene_lenght = gene_lenghts[gene]
+
+				rpkm = (10**9 * float(g))/ (float(gene_lenght) * float(lib_size))
+
+
+				#if rpkm >= 10 and e >= 10:
+				#if rpkm >= 10:
+
+				ratios.append(ratio)
+				phis.append(phi)
+				rpkms.append(rpkm)
+
+		CV_phis =  (100* np.std(phis)) / np.mean(phis)
+
+		if len(phis)>5:
+
+			# print cell, exon, gene, exon_number, ratios, phis, rpkms, CV_phis, np.mean(rpkms)
+
+			exon_number = "E" + exon_number
+
+			print exon, gene, exon_number, CV_phis, np.mean(rpkms), ",".join(ratios), ",".join(map(str, phis)), ",".join(map(str, rpkms))
+
+
+# python ~/my_src/PhD/Teichmann/DEXSeq_count_merge.py gencode.vM9.chr_patch_hapl_scaff.annotation.DEXseq.gff.sed i2m_* | sort 
+
+
+
---
+++ PhD/Teichmann/U2_score_DEXSeq.py
@@ -0,0 +1,265 @@
+import sys
+import csv
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
+from collections import defaultdict
+
+
+Genome = {}
+
+def Genomictabulator(fasta):
+	
+	print >> sys.stderr, "Cargando genoma en la memoria RAM ...",	
+
+	f = open(fasta)
+
+	for chrfa in SeqIO.parse(f, "fasta"):
+		Genome[chrfa.id] = chrfa.seq.split(" ")[0]
+		
+	print >> sys.stderr, "OK"
+
+	f.close()
+
+
+
+def percent (c, total):
+	try:
+		return (100*float(c))/float(total)
+	except ZeroDivisionError:
+		return 0	
+
+def PWM_to_dict(file):
+	reader = csv.reader(open(file), delimiter = '\t')
+	header = reader.next()
+	header_dict = {}
+	col = 0
+	
+	matrix = {}
+	
+	for name in header:
+		header_dict[name] = col
+		col += 1
+	
+	A_frec = []
+	C_frec = []
+	G_frec = []
+	T_frec = []
+	
+	for row in reader:
+		A = row[header_dict["A"]]
+		C = row[header_dict["C"]]
+		G = row[header_dict["G"]]
+		T = row[header_dict["T"]]
+		
+		A_frec.append(float(A))
+		C_frec.append(float(C))
+		G_frec.append(float(G))
+		T_frec.append(float(T))
+			
+	matrix["A"] = A_frec
+	matrix["C"] = C_frec
+	matrix["G"] = G_frec
+	matrix["T"] = T_frec
+	
+	return matrix
+
+
+def PWM_score(seq, PWM_dict):
+
+	PWM_max_score = 0
+	index = 0
+	for N in range(len(PWM_dict["A"])):
+		PWM_max_score += max(PWM_dict['A'][index], PWM_dict['C'][index], PWM_dict['T'][index], PWM_dict['G'][index])
+		index += 1
+
+
+	PWM_score = 0
+	index = 0
+	for N in seq:
+		try:
+			frec = PWM_dict[N][index]
+			PWM_score += frec
+			index += 1
+		except KeyError:
+			pass
+
+	percentual_PWM_score = percent(PWM_score, PWM_max_score)
+
+	return percentual_PWM_score
+
+
+def main(GTF, U2_GTAG_5_file, U2_GTAG_3_file, GFF_DEXSEQ):
+
+	U2_GTAG_5 = PWM_to_dict(U2_GTAG_5_file)
+	U2_GTAG_3 = PWM_to_dict(U2_GTAG_3_file)
+		
+	U2_GTAG_5_win = range(13)
+	U2_GTAG_5_max_score = 0
+	index = 0
+	for N in U2_GTAG_5_win:
+		U2_GTAG_5_max_score += max(U2_GTAG_5['A'][index], U2_GTAG_5['C'][index], U2_GTAG_5['T'][index], U2_GTAG_5['G'][index])
+		index += 1
+	
+	U2_GTAG_3_win = range(17)
+	U2_GTAG_3_max_score = 0
+	index = 0
+	for N in U2_GTAG_3_win:
+		U2_GTAG_3_max_score += max(U2_GTAG_3['A'][index], U2_GTAG_3['C'][index], U2_GTAG_3['T'][index], U2_GTAG_3['G'][index])
+		index += 1
+
+	transcripts_exons = defaultdict(list)
+
+	for row in csv.reader(filter(lambda row: row[0]!='#', open(GTF)), delimiter = '\t'):
+
+		chrom, source, feature, start, end, score, strand, frame, attribute =  row
+
+		
+		if feature=='exon':
+
+			transcript = attribute.split("; ")[1].split(" ")[1]
+			transcripts_exons[transcript].append(row)
+
+
+
+	donor_acceptor = {}
+	acceptor = {}
+
+	for i in transcripts_exons.items():
+
+		transcript, rows = i
+
+		if len(rows)>=3:
+
+			for row in rows[1:-1]:
+
+				chrom, source, feature, start, end, score, strand, frame, attribute =  row
+
+				start = int(start)
+				end = int(end)
+
+				estart = start - 1
+				eend = end
+
+				exon_5 = Genome[chrom][(estart-14):(estart+3)]
+				exon_3 = Genome[chrom][(eend-3):(eend+10)]
+
+
+				if strand=="-":
+
+					exon_5 = Genome[chrom][(eend-3):(eend+14)]
+					exon_3 = Genome[chrom][(estart-10):(estart+3)]
+
+					exon_5 = exon_5.reverse_complement()
+					exon_3 = exon_3.reverse_complement()
+
+				exon_5 = str(exon_5).upper()
+				exon_3 = str(exon_3).upper()
+
+				U2_GTAG_5_score = 0
+				index = 0 
+				for N in exon_3:
+					try:
+						frec = U2_GTAG_5[N][index]
+						U2_GTAG_5_score += frec
+						index += 1
+					except KeyError:
+						pass
+
+				U2_GTAG_3_score = 0
+				index = 0 
+				for N in exon_5:
+					try:
+						frec = U2_GTAG_3[N][index]
+						U2_GTAG_3_score += frec
+						index += 1					 
+					except KeyError:
+						pass
+
+				U2_GTAG_total_score = percent( (U2_GTAG_5_score + U2_GTAG_3_score), (U2_GTAG_5_max_score + U2_GTAG_3_max_score) )
+
+				U2_GTAG_5_score = percent( U2_GTAG_5_score, U2_GTAG_5_max_score )
+				U2_GTAG_3_score = percent( U2_GTAG_3_score, U2_GTAG_3_max_score )
+				chrom = chrom.strip("chr")
+
+				if strand == "+":
+
+					donor_acceptor["_".join(map(str, [chrom, start, strand]))] = "\t".join([str(U2_GTAG_3_score), "acceptor"])
+					donor_acceptor["_".join(map(str, [chrom, start-1, strand]))] = "\t".join([str(U2_GTAG_3_score), "acceptor"])
+
+					donor_acceptor["_".join(map(str, [chrom, end, strand]))] = "\t".join([str(U2_GTAG_5_score), "donor"])
+					donor_acceptor["_".join(map(str, [chrom, end+1, strand]))] = "\t".join([str(U2_GTAG_5_score), "donor"])
+
+
+				if strand == "-":
+
+					donor_acceptor["_".join(map(str, [chrom, end, strand]))] = "\t".join([str(U2_GTAG_3_score), "acceptor"])
+					donor_acceptor["_".join(map(str, [chrom, end+1, strand]))] = "\t".join([str(U2_GTAG_3_score), "acceptor"])
+
+					donor_acceptor["_".join(map(str, [chrom, start, strand]))] = "\t".join([str(U2_GTAG_5_score), "donor"])
+					donor_acceptor["_".join(map(str, [chrom, start-1, strand]))] = "\t".join([str(U2_GTAG_5_score), "donor"])
+
+
+
+	for row in csv.reader(open(GFF_DEXSEQ), delimiter = '\t'):
+
+
+		chrom, gff_file, feature, start, end, dot1, strand, dot2, IDs = row
+
+		start_score = []
+		end_score = []
+
+		if feature == "exonic_part":
+
+				gene = IDs.split(" ")[-1].strip('"')
+
+					
+
+				if strand == "+":
+
+					try:
+
+						start_score =  donor_acceptor["_".join([chrom, start, strand])]
+
+					except KeyError:
+						pass
+
+					try:
+
+						end_score = donor_acceptor["_".join([chrom, end, strand])]
+
+					except KeyError:
+						pass
+
+
+
+				if strand == "-":
+
+					try: 
+
+						start_score = donor_acceptor["_".join([chrom, end, strand])]
+
+					except KeyError:
+						pass 
+
+					try:
+
+						end_score = donor_acceptor["_".join([chrom, start, strand])]
+
+					except KeyError:
+						pass 
+
+
+
+		if start_score!=[] and end_score!=[]:
+
+			print "\t".join(row) + "\t" + start_score + "\t" + end_score
+
+
+
+if __name__ == '__main__':
+	Genomictabulator(sys.argv[1])
+	main(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5]) # , sys.argv[2], sys.argv[2])
+
+
+# 15 82109521 82109538 76.6534370656 []
