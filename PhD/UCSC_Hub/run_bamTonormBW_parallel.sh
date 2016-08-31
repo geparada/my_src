02b4f0b787590e34175abe15a752677dac969992
Guillermo Parada <geparada88@gmail.com>
2016-08-31 08:41:37 +0100

    bw_norm

--- PhD/UCSC_Hub/run_bamTonormBW_parallel.sh
+++ PhD/UCSC_Hub/run_bamTonormBW_parallel.sh
@@ -7,10 +7,10 @@ for i in $(ls *.bam)
 
 		name=$(basename $i .bam)
 
-		sed 's/name/$name/g' /Users/gp7/my_src/PhD/UCSC_Hub/bamTonormBW.sh > bamTonormBW.$name.sh
+		sed 's/name/$name/g' ~/my_src/PhD/UCSC_Hub/bamTonormBW.sh > bamTonormBW.$name.sh
 
 		chmod +x bamTonormBW.$name.sh
 
-		bash /Users/gp7/submit.job -s ./bamTonormBW.$name.sh -m 50000 -q normal -n bw.$name.norm
+		bash ~/submit.job -s ./bamTonormBW.$name.sh -m 50000 -q normal -n bw.$name.norm
 
 	done
