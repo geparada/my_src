7723fc9a11d1c8b744ae86521ff840b30cd4d93e
Guillermo Parada Gonzalez <gp7@sanger.ac.uk>
2017-02-09 13:05:35 +0000

    update

--- PhD/UCSC_Hub/normalize_bw.py
+++ PhD/UCSC_Hub/normalize_bw.py
@@ -4,15 +4,15 @@ import csv
 def main(bedGraph):
 
 
-	total_count = sum(int(row[3]) for row in csv.reader(open(bedGraph), delimiter = '\t'))
+	total_count = float(sum(int(row[3]) for row in csv.reader(open(bedGraph), delimiter = '\t')))
 
 	for row in csv.reader(open(bedGraph), delimiter = '\t'):
 
 	 	chrom, start, end, count = row
 
-	 	count = int(count)
+	 	count = float(count)
 
-	 	normalised_count = str(float(count/total_count)*10**6)
+	 	normalised_count = str((count/total_count)*10**6)
 
 	 	print "\t".join([chrom, start, end, normalised_count])
 
